c01440c2923a2b2abbfdb4b1a713e3633bd84dc8
Mozdalif sikder <mozsocia@gmail.com>
2024-10-10 20:10:59 +0600

    output dir

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import vue2 from '@vitejs/plugin-vue2'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue2()],
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm.js'
+    }
+  },
+  build: {
+    // outDir: path.resolve(__dirname, '../www/output'), // for output outside of project root
+    outDir: 'out',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'app.js',
+        assetFileNames: 'app.css'
+      }
     }
   }
 })
